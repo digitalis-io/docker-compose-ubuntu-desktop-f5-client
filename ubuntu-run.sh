77aa17a655412a65801498ab4d74e21ba9ed27e9
Johnny Miller <johnny.p.miller@gmail.com>
2025-01-25 15:55:08 +0100

    got basic working

---
+++ ubuntu-run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+start_xrdp_services() {
+    # Preventing xrdp startup failure
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/xrdp-sesman.pid
+    rm -rf /var/run/xrdp/xrdp.pid
+
+    # Use exec ... to forward SIGNAL to child processes
+    xrdp-sesman && exec xrdp -n
+}
+
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
+
+
+echo Entryponit script is Running...
+echo
+
+addgroup admin
+echo "username is admin"
+useradd -m -s /bin/bash -g admin admin
+wait
+#getent passwd | grep foo
+echo admin:admin | chpasswd 
+wait
+usermod -aG sudo admin
+
+
+echo -e "This script is ended\n"
+
+echo -e "starting xrdp services...\n"
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
